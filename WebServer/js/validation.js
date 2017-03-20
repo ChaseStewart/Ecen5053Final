00cf6272c6e75b586a3b08379e72c1ba2f262136
VTR-Records <vtrrecords@gmail.com>
2017-03-20 21:39:26 +0000

    Comment js

--- WebServer/js/validation.js
+++ WebServer/js/validation.js
@@ -1,23 +1,31 @@
 function validation(){
+         // For new user signup, ensure all fields are filled to satisfaction
+
+         // Don't allow empty username
          if( document.myForm.usernamesignup.value == "" )
          {
             alert( "Please provide your name!" );
             document.myForm.usernamesignup.focus() ;
             return false;
          }
-         
+
+         // Don't allow empty email
          if( document.myForm.emailsignup.value == "" )
          {
             alert( "Please provide your Email!" );
             document.myForm.emailsignup.focus() ;
             return false;
          }
+        
+         // Don't allow empty password
          if( document.myForm.passwordsignup.value == "" )
          {
             alert( "Please provide your password!" );
             document.myForm.passwordsignup.focus() ;
             return false;
          }
+
+         // don't allow empty password-confirm
          if( document.myForm.passwordsignup_confirm.value == "" )
          {
             alert( "Please retype the password!" );
@@ -30,12 +38,16 @@ function validation(){
          var password_c=document.myForm.passwordsignup_confirm.value;
          atpos = emailID.indexOf("@");
          dotpos = emailID.lastIndexOf(".");  
+
+         // ensure email entry has <something>@<something>.<something>
          if (atpos < 1 || ( dotpos - atpos < 2 )) 
          {
             alert("Please enter correct email ID")
             document.myForm.emailsignup.focus() ;
             return false;
          }
+
+        // Don't allow non_matching password and password-confirm
         if(password === password_c)
          {
             return(true);
