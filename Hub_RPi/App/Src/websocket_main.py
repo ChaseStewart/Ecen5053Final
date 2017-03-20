46042abdf31385ca434ab461f6ff2de4623d85ab
Mounika1494 <edulamounika08@gmail.com>
2017-03-20 00:36:36 -0600

    Update websocket_main.py
    working labels are updated

--- Hub_RPi/App/Src/websocket_main.py
+++ Hub_RPi/App/Src/websocket_main.py
@@ -5,6 +5,7 @@ from tornado.ioloop import IOLoop, PeriodicCallback
 from tornado import gen
 from tornado.websocket import websocket_connect
 from PyQt4 import QtGui, QtCore
+from PyQt4.QtCore import QTimer
 import sys
 
 
@@ -14,22 +15,42 @@ class Hub(QtGui.QMainWindow):
         super(Hub,self).__init__()
         self.url = "ws://52.34.209.113:8080/websocket"
         self.timeout = 5
+        self.first_time=True
         self.ioloop = IOLoop.instance()
         self.ws = None
         self.initUI()
         self.connect()
-	self.my_p_callback = PeriodicCallback(self.keep_alive, 20000, io_loop=self.ioloop)
+	self.my_p_callback = PeriodicCallback(self.keep_alive, 1000, io_loop=self.ioloop)
 	self.my_p_callback.start()
         self.ioloop.start()
         
+        
     def initUI(self):
-        Disarm_btn=QtGui.QPushButton("Disarm",self)
-        Disarm_btn.move(100,400)
-        Disarm_btn.setFixedSize(150,50)
+        Disarm_btn=QtGui.QPushButton("Arm",self)
+        #Disarm_btn.move(100,400)
+        #Disarm_btn.setFixedSize(150,50)
         self.user_data=QtGui.QLabel(self)
+        self.user_data.setText("Name")
+        self.time=QtGui.QLabel(self)
+        self.time.setText("Time")
         self.user_data.setFixedSize(200,50)
         self.user_data.move(0,100)
+        self.time.setFixedSize(300,50)
+        self.time.move(100,100)
         Disarm_btn.clicked.connect(self.sendDisarm)
+        wid = QtGui.QWidget(self)
+        self.setCentralWidget(wid)
+        grid = QtGui.QGridLayout()
+        grid.setSpacing(10)
+        vbox = QtGui.QVBoxLayout()
+        vbox.addWidget(Disarm_btn)
+        hbox= QtGui.QHBoxLayout()
+        hbox.addWidget(self.user_data)
+        hbox.addWidget(self.time)
+        grid.addLayout(hbox,0,0,1,1)
+        grid.addLayout(vbox,0,1,1,1)
+        wid.setLayout(grid)
+
         self.setGeometry(50,50,600,400)
         self.setWindowTitle('Welcome')
         self.show()
@@ -50,25 +71,54 @@ class Hub(QtGui.QMainWindow):
             print "connection error"
         else:
             print "connected"
-            self.ws.write_message("login_status")
-            self.ws.write_message("arm_status")
-            self.run()
+
+
+    def sendAndRead(self):
+        print("Sending messages!")
+        self.ws.write_message("login_status")
+        self.readInput()
+        self.ws.write_message("arm_status")
+        self.readInput()
 
     @gen.coroutine
-    def run(self):
-        print "run"
-        msg = yield self.ws.read_message()
-        self.user_data.setText(msg)
-        print msg
+    def readInput(self):
+        print "reading input"
+        
+        try:
+            msg = yield self.ws.read_message()
+        except:
+            return
+
+        indata = msg.split(":")
+        if indata[0] == "name" :
+            self.user_data.setText("Welcome, "+indata[1])
+        elif indata[0] == "state" :
+            self.time.setText("Arm/Disarm state is: "+indata[1])
+        print indata[1]
         
     def keep_alive(self):
-        if not self.my_p_callback.is_running():
-            self.ioloop.start()
         if self.ws is None:
+            print "reconnecting in keepalive"
             self.connect()
         else:
-            self.run()
-        self.ioloop.stop()
+            #print "in keep alive"
+            if self.first_time:
+                self.first_time = False
+            else:
+                try:
+                    self.ioloop.start()
+                except:
+                    pass
+            self.sendAndRead()
+            #self.timer.start(100)
+            self.ioloop.stop()
+            self.my_p_callback.stop()
+            QTimer.singleShot(500, self.hail_mary_pass)
+            
+
+    def hail_mary_pass(self):
+        self.my_p_callback.start()
+        self.ioloop.start()
 
 
 if __name__ == "__main__":
