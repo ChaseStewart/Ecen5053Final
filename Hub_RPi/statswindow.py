184205ac128709b535e93590e7ac47fd9649cae8
VTR-Records <vtrrecords@gmail.com>
2017-05-03 01:39:51 -0600

    stats working

--- Hub_RPi/statswindow.py
+++ Hub_RPi/statswindow.py
@@ -6,6 +6,7 @@ from PyQt4 import QtGui, QtCore
 from PyQt4.QtCore import QTimer
 from helpers import WindowState
 from helpers import GraphState
+from time import time
 import numpy as np
 
 # MatPlotLib setup
@@ -13,6 +14,10 @@ import matplotlib
 from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
 
+import paho.mqtt.publish as publish
+import websocket
+
+from coapthon.client.helperclient import HelperClient
 
 class graphCanvas(FigureCanvas):
 	"""
@@ -65,7 +70,7 @@ class DynamicGraph(graphCanvas):
 		
 		self.ax3.plot(range(10), [np.NaN]*10,'r')
 		self.axes.set_ylabel('axis 1', color='r')
-		self.axes.set_ylim(-1,1)
+		self.axes.set_ylim(-1,2)
 
 		self.axes.set_xlabel('Sample')
 		self.axes.set_title('Choose a stat to graph')
@@ -74,13 +79,13 @@ class DynamicGraph(graphCanvas):
 		
 		self.ax2.plot(range(10), [np.NaN]*10, 'b')
 		self.ax2.set_ylabel('axis 2', color='b')
-		self.ax2.set_ylim(-1,1)
-		self.ax3.set_ylim(-1,1)
-		self.ax4.set_ylim(-1,1)
+		self.ax2.set_ylim(-1,2)
+		self.ax3.set_ylim(-1,2)
+		self.ax4.set_ylim(-1,2)
 
 
 
-	def update_figure(self, graph_type, array1, array2, array3=None ):
+	def update_figure(self, graph_type, led1array, led2array, array3=None ):
 		"""
 		Update the line chart w/ data from the caller
 		"""
@@ -100,54 +105,43 @@ class DynamicGraph(graphCanvas):
                         self.axes.set_title('Usage of Lights ')
 	
                         self.ax2.set_ylabel('Lights Off', color='b')
-                        self.ax2.set_ylim(-1,1)
-                        self.ax2.plot(range(10), array2, 'b')
+                        self.ax2.set_ylim(-1,2)
+                        self.ax2.plot(range(10), led2array, 'b')
 
-                        self.ax3.set_ylim(-1,1)
-                        self.ax3.plot(range(10),array1,'r')
+                        self.ax3.set_ylim(-1,2)
+                        self.ax3.plot(range(10),led1array,'r')
                         
                 if graph_type == GraphState.OVERHEAD:
 
         		self.ax4 = self.axes.twinx()
                         
-                        self.axes.set_ylabel('overhead(bytes) ', color='r')
-                        self.axes.set_title('Size of Overhead for all protocols ')
-                        self.axes.set_ylim(1,50)
+                        #self.axes.set_ylabel('overhead(bytes) ', color='r')
+                        self.axes.set_title('Latency for all protocols ')
+                        self.axes.set_ylabel('Latency', color='k')
 
 
-                        self.axes.set_ylabel('MQTT', color='b')
-                        self.axes.set_ylim(1,50)
-                        self.ax3.plot(range(10),array1,'r')
+                        self.ax3.plot(range(10),led1array,'r')
+                        self.ax3.set_ylim(0,1)
 		
-                        self.ax2.set_ylabel('Websockets', color='b')
-                        self.ax2.set_ylim(1,50)
-                        self.ax2.plot(range(10), array2, 'b')
+                        self.ax2.set_ylim(0,1)
+                        self.ax2.plot(range(10), led2array, 'b')
 
-                        self.axes.set_ylabel('CoAP', color='b')
-                        self.ax3.set_ylim(1,50)
-                        self.ax4.set_ylim(1,50)
+                        self.ax4.set_ylim(0,1)
                         self.ax4.plot(range(10), array3, 'g')
                         
                 if graph_type == GraphState.LATENCY:
         		self.ax4 = self.axes.twinx()
-                
-                        self.axes.set_ylabel('LATENCY ', color='r')
+                        
                         self.axes.set_title('Latency for all protocols ')
+                        self.axes.set_ylabel('Latency', color='k')
+                        
+			self.ax2.set_ylim(0,1)
+                        self.ax2.plot(range(10), led2array, 'b')
 
+                        self.ax3.plot(range(10),led1array,'r')
+                        self.ax3.set_ylim(0,1)
 
-                        # unfortunately this means replacing limits and labels w/ data
-                        self.axes.set_ylabel('MQTT', color='b')
-                        self.axes.set_ylim(0,10)
-                        self.ax3.plot(range(10),array1,'r')
-		
-                        # same goes for lights_of data
-                        self.ax2.set_ylabel('Websockets', color='b')
-                        self.ax2.set_ylim(0,10)
-                        self.ax2.plot(range(10), array2, 'b')
-
-                        #
-                        self.axes.set_ylabel('CoAP', color='b')
-                        self.axes.set_ylim(0,10)
+                        self.ax4.set_ylim(0,1)
                         self.ax4.plot(range(10), array3, 'g')
 
                 self.draw()
@@ -175,10 +169,15 @@ class StatsWindow(QtGui.QMainWindow):
         self.graph_type = None
         
         # measurement data vars
-	self.array1    = [0]*10
-	self.array2    = [0]*10
-	self.array3    = [0]*10
+	self.led1array    = [np.NaN]*10
+	self.led2array    = [np.NaN]*10
+	self.array3       = [np.NaN]*10
 	self.plotCount = 0
+
+        self.wsArray   = [np.NaN] * 10 
+	self.mqttArray = [np.NaN] * 10
+	self.coapArray = [np.NaN] * 10
+	self.latencyCount = 0
 
         self.initUI()
 
@@ -260,23 +259,41 @@ class StatsWindow(QtGui.QMainWindow):
 
 	# When count < len(graph), just populate graph
 	if self.plotCount < 10:
-                self.array1[self.plotCount] = self.lights_on
-		self.array2[self.plotCount] = self.lights_of
+                self.led1array[self.plotCount] = self.lights_on
+		self.led2array[self.plotCount] = self.lights_of
 		self.plotCount += 1	
 			
 	# then keep rolling the chart over
 	else:
-                self.array1[0] = self.lights_on
-		self.array1 = np.roll(self.array1, len(self.array1)-1)		
-		self.array2[0] = self.lights_of
-		self.array2 = np.roll(self.array2, len(self.array2)-1)
+                self.led1array[0] = self.lights_on
+		self.led1array = np.roll(self.led1array, len(self.led1array)-1)		
+		self.led2array[0] = self.lights_of
+		self.led2array = np.roll(self.led2array, len(self.led2array)-1)
 
-	self.canvas.update_figure(GraphState.LIGHTS, self.array1, self.array2)
+	self.canvas.update_figure(GraphState.LIGHTS, self.led1array, self.led2array)
 
 
-    def latencyStats(self):
-        print("Not implemented! Latency")
-	#self.canvas.update_figure(GraphState.LATENCY, self.ws_latency, self.coap_latency, self.mqtt_latency)
+    def latencyStats(self, mqtt, coap, ws):
+	
+	# When count < len(graph), just populate graph
+	if self.latencyCount < 10:
+                self.wsArray[self.latencyCount]   = ws
+		self.mqttArray[self.latencyCount] = mqtt
+		self.coapArray[self.latencyCount] = coap
+		self.latencyCount += 1	
+			
+	# then keep rolling the chart over
+	else:
+                self.wsArray[0] = ws
+		self.wsArray = np.roll(self.wsArray, len(self.wsArray)-1)		
+
+		self.coapArray[0] = coap
+		self.coapArray = np.roll(self.coapArray, len(self.coapArray)-1)
+		
+                self.mqttArray[0] = mqtt
+		self.mqttArray = np.roll(self.mqttArray, len(self.mqttArray)-1)
+        
+	self.canvas.update_figure(GraphState.LATENCY, self.wsArray, self.coapArray, self.mqttArray)
         return
 
 
@@ -293,10 +310,9 @@ class StatsWindow(QtGui.QMainWindow):
 	Start the timer with timeout set to latency calculation
 	"""	
 	self.timer.stop()
-        self.timer =  QTimer(self)
-
+        self.timer =  QTimer()
        	self.timer.timeout.connect(self.overheadStats)
-	self.timer.start(1000)
+	self.timer.start(3000)
         return
 
 
@@ -305,9 +321,9 @@ class StatsWindow(QtGui.QMainWindow):
 	Start the timer with timeout set to latency calculation
 	"""	
 	self.timer.stop()
-	self.timer =  QTimer(self)
-       	self.timer.timeout.connect(self.latencyStats)
-	self.timer.start(1000)
+	self.timer =  QTimer()
+       	self.timer.timeout.connect(self.startTests)
+	self.timer.start(3000)
         return
 
 
@@ -318,10 +334,39 @@ class StatsWindow(QtGui.QMainWindow):
 	self.timer.stop()
 	self.timer =  QTimer(self)
        	self.timer.timeout.connect(self.ledStats)
-	self.timer.start(1000)
+	self.timer.start(3000)
         return
 
 
+    def startTests(self):
+
+        self.testMQTT()
+        self.testWebsockets()
+        self.testCoAP()
+	print "tests complete!"
+        return
+
+
+    def testCoAP(self):
+        host = '52.34.209.113'
+        port = 5683
+        path = 'test'
+        client = HelperClient(server=(host, port))
+        curr_time = time()
+	try:
+        	response = client.put(path, str(curr_time), timeout=0.1)
+	except Exception as e:
+		print("Exception: %s" % e)
+        client.stop()
+
+    def testMQTT(self):
+        curr_time = time()
+        publish.single("AccessControl/MQTT_test", str(curr_time), hostname="test.mosquitto.org") 
+
+    def testWebsockets(self):
+        ws = websocket.create_connection("ws://52.34.209.113:8000")
+        curr_time = time()
+	ws.send(str(curr_time))
 
     def goBack(self):
         self.parent.window_state = WindowState.MAIN_WINDOW
